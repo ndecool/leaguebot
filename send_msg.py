efc0836c09f8c5b814d7c51d8f6a411ac976d4ea
ndecool <decoolnath@gmail.com>
2020-12-28 19:28:48 +0100

    Create send_msg.py

---
+++ send_msg.py
@@ -0,0 +1,14 @@
+import telepot
+import sys
+mqueue=""
+split=sys.argv[1].split(',')
+lenght=len(split)
+
+for   y in range(lenght): 
+    if y ==  0:
+        mqueue = mqueue + ":fr:"+ split[y]+ "\n"
+    else : 
+        mqueue = mqueue + split[y]+ "\n"
+print(mqueue)
+bot = telepot.Bot('KEY BOT')
+bot.sendMessage(yourr own id,mqueue)
